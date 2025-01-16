bd38c2b6fb2facd539b64b5179ebf5d91ed4a2b6
arachid1 <alilerachidi@gmail.com>
2025-01-16 15:47:44 -0500

    refactoring tests + adding repeatibility test (off by a small margin based on jake's example in his repo)

--- tests/test_mock_execution.py
+++ tests/test_mock_execution.py
@@ -4,59 +4,6 @@ from alpss.alpss_main import alpss_main
 import matplotlib.pyplot as plt
 
 
-# Mocking the inputs dictionary and the required functions
-@pytest.fixture
-def valid_inputs():
-    return {
-        "filepath": "input_data/example_file.csv",
-        "save_data": "yes",
-        "start_time_user": "none",
-        "header_lines": 0,
-        "time_to_skip": 0e-6,
-        "time_to_take": 10e-6,
-        "t_before": 10e-9,
-        "t_after": 200e-9,
-        "start_time_correction": 0e-9,
-        "freq_min": 1e9,
-        "freq_max": 5e9,
-        "smoothing_window": 1001,
-        "smoothing_wid": 3,
-        "smoothing_amp": 1,
-        "smoothing_sigma": 1,
-        "smoothing_mu": 0,
-        "pb_neighbors": 400,
-        "pb_idx_correction": 0,
-        "rc_neighbors": 400,
-        "rc_idx_correction": 0,
-        "sample_rate": 128e9,
-        "nperseg": 512,
-        "noverlap": 435,
-        "nfft": 5120,
-        "window": "hann",
-        "blur_kernel": (5, 5),
-        "blur_sigx": 0,
-        "blur_sigy": 0,
-        "carrier_band_time": 250e-9,
-        "cmap": "viridis",
-        "uncert_mult": 100,
-        "order": 6,
-        "wid": 15e7,
-        "lam": 1550.016e-9,
-        "C0": 4540,
-        "density": 1730,
-        "delta_rho": 9,
-        "delta_C0": 23,
-        "delta_lam": 8e-18,
-        "theta": 0,
-        "delta_theta": 5,
-        "out_files_dir": "output_data/",
-        "display_plots": "no",  # Overwritten from the original value
-        "spall_calculation": "yes",
-        "plot_figsize": (30, 10),
-        "plot_dpi": 300,
-    }
-
-
 # Test for valid inputs and outputs, assuming the function runs correctly
 def test_alpss_main_success(valid_inputs):
     with patch("alpss.alpss_main.spall_doi_finder") as mock_spall_doi_finder, patch(
